5f37775087bcbca15d8fd9d6fcf65ea0442154c3
yunuskaygun <yunuskaygun@gmail.com>
2017-12-26 12:45:06 +0300

    FirebaseAdminSdkNodeJSExample
    Firebase Admin SDK NodeJs Example.
    
    Note: Add your admin sdk json file to project (serviceAccountKey.json)

---
+++ index.js
@@ -0,0 +1,68 @@
+﻿var sys = require("util");
+
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./serviceAccountKey.json");
+
+admin.initializeApp(
+{        
+      credential: admin.credential.cert(
+            serviceAccount
+      ),
+      databaseURL: "https://piri-vehicle-service.firebaseio.com"
+});
+var say = 0;
+
+//setCustomUserClaims("U8m8STUAqeUaqVDKB5aKWMm5s842");
+
+function setCustomUserClaims(uid) {
+      admin.auth().setCustomUserClaims(uid, {roleId: 1}).then(() => {
+            console.log("İşlem Tamam: " + uid);      
+      });
+}
+createUser();
+function createUser() {
+      admin.auth().createUser(
+            {
+                  email: "user@example.com",
+                  emailVerified: false,
+                  phoneNumber: "+11234567890",
+                  password: "123456",
+                  displayName: "John Doe",
+                  photoURL: "http://www.example.com/12345678/photo.png",
+                  disabled: false,
+                  customClaims: { roleId: 1 }
+                }
+
+      ).then(() => {
+            console.log("İşlem Tamam: ");      
+      });
+}
+
+function DoSomething(uid){
+      setTimeout(function() { setCustomUserClaims(uid); }, 1000 * say);
+}
+
+
+
+function listAllUsers() {
+      
+      // List batch of users, 1000 at a time.
+      admin.auth().listUsers(1000)
+        .then(function(listUsersResult) {
+          listUsersResult.users.forEach(function(userRecord) {
+            console.log(userRecord);
+            console.log("\n-----");
+          });
+          console.log("Toplam: " + say);
+          if (listUsersResult.pageToken) {
+            // List next batch of users.
+            //listAllUsers(listUsersResult.pageToken)
+          }
+        })
+        .catch(function(error) {
+          console.log("Error listing users:", error);
+        });
+    }
+
+    //listAllUsers();
